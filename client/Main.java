32177d19acfe904e1c9756a963644dc1173d01ed
Mr.Shtein <mr.shtein61@yandex.ru>
2021-04-24 15:40:11 +0300

    Main methods

--- client/Main.java
+++ client/Main.java
@@ -3,6 +3,6 @@ package client;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
     }
 }
--- server/Main.java
+++ server/Main.java
@@ -3,27 +3,7 @@ package server;
 public class Main {
 
     public static void main(String[] args) {
-        Db db = new Db();
-
-        boolean work = true;
-        DataGrabber dg = new DataGrabber();
-        while (dg.handleData()) {
-            try {
-                switch (dg.getMethod()) {
-                    case "get":
-                        System.out.println(db.getValue(Integer.parseInt(dg.getNum())));
-                        break;
-                    case "set":
-                        db.setValue(Integer.parseInt(dg.getNum()), dg.getValue());
-                        break;
-                    case "delete":
-                        db.deleteValue(Integer.parseInt(dg.getNum()));
-                        break;
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-
-        }
+        Server server = new Server();
+        server.start();
     }
 }
